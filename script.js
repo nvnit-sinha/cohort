c5b1e886fc4f69455943be29ab35f8e8685e5c30
Navneet <navneetsinha0307@gmail.com>
2025-12-02 16:53:49 +0530

    piano

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let keys = document.querySelectorAll(".keys");
-
+let h1 = document.querySelector("h1");
 let num = 28;
 keys.forEach((elem,index)=>{
     elem.textContent = String.fromCharCode(65+index);
@@ -7,6 +7,7 @@ keys.forEach((elem,index)=>{
     elem.addEventListener("click", ()=>{
         let aud = new Audio(`sounds/${soundNum}.mp3`);
         aud.play();
+        h1.textContent = "Great!!"
     });
     num++;
 });
